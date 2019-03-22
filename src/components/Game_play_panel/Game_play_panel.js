e1e33dfd99afa072aef1f54b7a991a436f1c7565
Sara Waclawiec <sara.ems.94@gmail.com>
2019-03-22 20:16:29 +0100

    scratch inner layout elements

--- src/components/Game_play_panel/Game_play_panel.js
+++ src/components/Game_play_panel/Game_play_panel.js
@@ -7,10 +7,18 @@ class GamePlayPanel extends Component {
             <Fragment>
                 <section className='main_game_panel_container'>
                     <header className='main_game_panel__header'>
-                        <h1>Kahhoot!</h1> POINTS
+                        <h1>Kahoot!</h1> <p>POINTS</p>
                     </header>
                     <main className='main_game_panel__body'>
+                        <div className='item'>
 
+                        </div>
+                        <div className="item">
+
+                        </div>
+                        <div className="item">
+
+                        </div>
                     </main>
                 </section>
             </Fragment>
--- src/components/Game_score_panel/Game_score_panel.js
+++ src/components/Game_score_panel/Game_score_panel.js
@@ -8,14 +8,35 @@ class GameScorePanel extends Component {
                 <div className="score_game_panel__header">
                     Players Items
                 </div>
+                <div className="main_game_panel__body_header">
+                    <p>item</p>
+                    <p>qty</p>
+                    <p>score</p>
+                </div>
                 <div className='score_game_panel__body'>
+                    <div className="collected_item">
+                        <div className="collected_item__icon">
 
+                        </div>
+                        <p className="collected_items__quantity"> 4 </p>
+                        <p className="collected_items__score"> 90 </p>
+                    </div>
+                    <div className="collected_item">
+
+                    </div>
+                    <div className="collected_item">
+
+                    </div>
+                    <div className="collected_item">
+
+                    </div>
                 </div>
                 <div className='score_game_panel__bonus'>
-
+                    Bonuses <span id='players_bonus'>90</span>
                 </div>
                 <div className="score_game_panel__resume_game">
-
+                    <div className="total">Total <span id='total_score'>902</span></div>
+                    <button className='new_game_btn'>New Game</button>
                 </div>
             </section>
         );
